29ddeee5d18d2941e7bb175fc34f2835e127b856
jaynylljacosalem <jaynyll.jacosalem@splitsecondsoftware.com>
2024-04-24 21:07:00 +0800

    add jellibean branch

--- services/nginx/entrypoint.sh
+++ services/nginx/entrypoint.sh
@@ -9,37 +9,46 @@ echo "***********************************************************"
 
 set -e
 
+# Check jellibean npm dependencies
+if [ ! -d /var/www/jellibean/test/node_modules ]; then
+    echo "Installing npm dependencies for jellibean...."
+    cd /var/www/jellibean.test && npm i
+fi
+
 # Check fluent npm dependencies
-if [ ! -d /var/www/fluent/test/node_modules ]; then
-    echo "Installing npm dependencies for fluent...."
-    cd /var/www/fluent.test && npm i
-fi
-# Check express npm dependencies
-if [ ! -d /var/www/express/test/node_modules ]; then
-    echo "Installing npm dependencies for express...."
-    cd /var/www/express.test && npm i
-fi
-# Check act npm dependencies
-if [ ! -d /var/www/act/test/node_modules ]; then
-    echo "Installing npm dependencies for act...."
-    cd /var/www/act.test && npm i
-fi
-# Check fast npm dependencies
-if [ ! -d /var/www/fast/test/node_modules ]; then
-    echo "Installing npm dependencies for fast...."
-    cd /var/www/fast.test && npm i
-fi
+# if [ ! -d /var/www/fluent/test/node_modules ]; then
+#     echo "Installing npm dependencies for fluent...."
+#     cd /var/www/fluent.test && npm i
+# fi
+# # Check express npm dependencies
+# if [ ! -d /var/www/express/test/node_modules ]; then
+#     echo "Installing npm dependencies for express...."
+#     cd /var/www/express.test && npm i
+# fi
+# # Check act npm dependencies
+# if [ ! -d /var/www/act/test/node_modules ]; then
+#     echo "Installing npm dependencies for act...."
+#     cd /var/www/act.test && npm i
+# fi
+# # Check fast npm dependencies
+# if [ ! -d /var/www/fast/test/node_modules ]; then
+#     echo "Installing npm dependencies for fast...."
+#     cd /var/www/fast.test && npm i
+# fi
+
+# Run npm run dev for jellibean
+(cd /var/www/jellibean.test && npm run dev) &
 
 # Run npm run dev for fluent
-(cd /var/www/fluent.test && npm run dev) &
+# (cd /var/www/fluent.test && npm run dev) &
 
-# Run npm run dev for express
-(cd /var/www/express.test && npm run dev) &
+# # Run npm run dev for express
+# (cd /var/www/express.test && npm run dev) &
 
-# Run npm run dev for act
-(cd /var/www/act.test && npm run dev) &
+# # Run npm run dev for act
+# (cd /var/www/act.test && npm run dev) &
 
-# Run npm run dev for fast
-(cd /var/www/fast.test && npm run dev) &
+# # Run npm run dev for fast
+# (cd /var/www/fast.test && npm run dev) &
 
 /usr/sbin/nginx -g "daemon off;"
