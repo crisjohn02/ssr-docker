48619143d3499e469347299c581bf4ccec37cfca
cris <crisjohnreytarpin@gmail.com>
2024-05-07 09:23:52 +0100

    fluent and act only

--- services/nginx/entrypoint.sh
+++ services/nginx/entrypoint.sh
@@ -14,41 +14,17 @@ if [ ! -d /var/www/fluent/test/node_modules ]; then
     echo "Installing npm dependencies for fluent...."
     cd /var/www/fluent.test && npm i
 fi
-# Check express npm dependencies
-if [ ! -d /var/www/express/test/node_modules ]; then
-    echo "Installing npm dependencies for express...."
-    cd /var/www/express.test && npm i
-fi
+
 # Check act npm dependencies
 if [ ! -d /var/www/act/test/node_modules ]; then
     echo "Installing npm dependencies for act...."
     cd /var/www/act.test && npm i
 fi
-# Check fast npm dependencies
-if [ ! -d /var/www/fast/test/node_modules ]; then
-    echo "Installing npm dependencies for fast...."
-    cd /var/www/fast.test && npm i
-fi
-
-# Check if the node_modules folder exists in impress
-if [ ! -d /var/www/impress/test/node_modules ]; then
-    echo "Installing npm dependencies for impress...."
-    cd /var/www/impress.test && npm i
-fi
 
 # Run npm run dev for fluent
 (cd /var/www/fluent.test && npm run dev) &
 
-# Run npm run dev for express
-(cd /var/www/express.test && npm run dev) &
-
 # Run npm run dev for act
 (cd /var/www/act.test && npm run dev) &
-
-# Run npm run dev for fast
-(cd /var/www/fast.test && npm run dev) &
-
-# Run npm run dev for impress
-(cd /var/www/impress.test && npm run dev) &
 
 /usr/sbin/nginx -g "daemon off;"
